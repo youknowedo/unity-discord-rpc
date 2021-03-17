f7278e029387b8a8c9a90216d247edfa3201cc3d
Fenwikk <56821719+fenwikk@users.noreply.github.com>
2021-03-17 11:24:46 +0100

    Added Lahee/discord-rpc-csharp

---
+++ Assets/Discord RPC/Example/Rich Presence/Example_RichPresence.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DiscordRPC.Examples.RichPresence
+{
+	public class Example_RichPresence : MonoBehaviour
+	{
+
+		[Header("Details")]
+		public InputField inputDetails, inputState;
+
+		[Header("Time")]
+		public Toggle inputStartTime;
+		public InputField inputEndTime;
+
+		[Header("Images")]
+		public InputField inputLargeKey;
+		public InputField inputLargeTooltip;
+		public InputField inputSmallKey;
+		public InputField inputSmallTooltip;
+
+		private DiscordManager current;
+
+		private void Start()
+		{
+			//Get the current instance of DiscordManager
+			current = GameObject.Find("Discord Manager").GetComponent<DiscordManager>();
+
+
+			//Update the values
+			UpdateFields();
+
+			//Register to a presence change
+			current.events.OnPresenceUpdate.AddListener((message) =>
+			{
+				Debug.Log("Received a new presence! Current App: " + message.ApplicationID + ", " + message.Name);
+				UpdateFields();
+			});
+		}
+
+		public void SendPresence()
+		{
+			current.UpdatePresence
+				(
+					details: inputDetails.text,
+					state: inputState.text,
+
+					start: inputStartTime.isOn,
+					endTime: Int32.Parse(inputEndTime.text),
+
+					largeKey: inputLargeKey.text,
+					largeText: inputLargeTooltip.text,
+					smallKey: inputSmallKey.text,
+					smallText: inputSmallTooltip.text
+				);
+			current.SetPresence(current.UnsavedPresence);
+		}
+
+		public void UpdateFields()
+		{
+			inputState.text = current.UnsavedPresence.state;
+			inputDetails.text = current.UnsavedPresence.details;
+
+
+			inputLargeTooltip.text = current.UnsavedPresence.largeAsset.tooltip;
+			inputLargeKey.text = current.UnsavedPresence.largeAsset.image;
+
+			inputSmallTooltip.text = current.UnsavedPresence.smallAsset.tooltip;
+			inputSmallKey.text = current.UnsavedPresence.smallAsset.image;
+		}
+	}
+}
